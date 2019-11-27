34d0c5086f605823b9accfe88e8aa82737a8bb4d
郑春在 <zhengchunzai@zhengchunzaideMacBook-Pro.local>
2019-11-27 18:19:55 +0800

    解决箭头函数在class中无效的问题

--- webpack4+/webpack.base.js
+++ webpack4+/webpack.base.js
@@ -49,7 +49,8 @@ module.exports = {
                             [
                                 "@babel/plugin-transform-runtime",//设定局部变量，防止多模块不兼容的问题
                                 { "corejs": 2 }//设定版本
-                            ]
+                            ],
+                            '@babel/plugin-proposal-class-properties'//解决箭头函数在class中无效的问题
                         ]
                     }
                 },
