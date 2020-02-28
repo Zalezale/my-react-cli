e808552dcbaa8f51906381206b55fc4bc7219d47
郑春在 <zhengchunzai@zhengchunzaideMacBook-Pro.local>
2020-02-28 16:38:24 +0800

    声明并赋值react中的props

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import './style.less'
+import PropTypes from 'prop-types'
 class Footer extends Component {
+    static propTypes = {
+        theme: PropTypes.object,
+    }
     render() {
         console.log(this.props.theme)
         setTimeout(() => {
             b
-        }, 0);
+        }, 0)
         console.log(this.props.theme)
         return <div className='footer' style={{ background: this.props.theme.color }}>
             <h5>我是底部哦</h5>
         </div>
     }
+}
+
+Footer.defaultProps = {
+    theme: { color: 'yellow' }
 }
 export default Footer
