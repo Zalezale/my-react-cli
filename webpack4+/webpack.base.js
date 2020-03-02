ed776dac533ff1b57f25b09db2656c9f7d706da3
郑春在 <zhengchunzai@zhengchunzaideMacBook-Pro.local>
2020-03-02 15:52:20 +0800

    添加eslint脚本

--- webpack4+/webpack.base.js
+++ webpack4+/webpack.base.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
 
@@ -34,8 +34,8 @@ module.exports = {
             cacheGroups: {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
+                    name: 'vendors',
+                    chunks: 'all'
                 }
             }
         }
@@ -48,27 +48,27 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        "presets": [
+                        'presets': [
                             [
-                                "@babel/preset-env",
+                                '@babel/preset-env',
                                 {
-                                    "targets": {
-                                        "edge": "17",
-                                        "firefox": "60",
-                                        "chrome": "67",
-                                        "safari": "11.1"
+                                    'targets': {
+                                        'edge': '17',
+                                        'firefox': '60',
+                                        'chrome': '67',
+                                        'safari': '11.1'
                                     },
-                                    "useBuiltIns": "usage",//只装载需要的api
-                                    "corejs": 2 //明确版本
+                                    'useBuiltIns': 'usage',//只装载需要的api
+                                    'corejs': 2 //明确版本
 
                                 },
-                            ], "@babel/preset-react"//用于解析jsx
+                            ], '@babel/preset-react'//用于解析jsx
 
                         ],
-                        "plugins": [
+                        'plugins': [
                             [
-                                "@babel/plugin-transform-runtime",//设定局部变量，防止多模块不兼容的问题
-                                { "corejs": 2 }//设定版本
+                                '@babel/plugin-transform-runtime',//设定局部变量，防止多模块不兼容的问题
+                                { 'corejs': 2 }//设定版本
                             ],
                             '@babel/plugin-proposal-class-properties'//解决箭头函数在class中无效的问题
                         ]
@@ -114,4 +114,4 @@ module.exports = {
             }
         ]
     }
-};
+}
