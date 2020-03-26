23d17457dde523ee0f71c3c59c99230210fe8d62
郑春在 <zhengchunzai@192.168.5.5>
2020-03-26 21:16:25 +0800

    修改图片的引入方式

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import banner from '../../assets/imgs/banner.png'
+import buttonBackground from '../../assets/imgs/button.png'
 import './style'
 class HomePage extends Component {
     cat = (path) => {
@@ -12,11 +13,11 @@ class HomePage extends Component {
         return <div className='home-page'>
             <img className='logo' src={banner} />
             <div className='actions'>
-                <button onClick={() => this.cat('/report')}>客户报备</button>
-                <button onClick={() => this.cat('/reportlog')}>报备记录</button>
-                <button onClick={() => this.cat('/myclient')}>我的客户</button>
-                <button onClick={() => this.cat('/prolist')}>项目介绍</button>
-                <button onClick={() => this.cat('/prolist')}>公告信息</button>
+                <button style={{ backgroundImage: `url(${buttonBackground})` }} onClick={() => this.cat('/report')}>客户报备</button>
+                <button style={{ backgroundImage: `url(${buttonBackground})` }} onClick={() => this.cat('/reportlog')}>报备记录</button>
+                <button style={{ backgroundImage: `url(${buttonBackground})` }} onClick={() => this.cat('/myclient')}>我的客户</button>
+                <button style={{ backgroundImage: `url(${buttonBackground})` }} onClick={() => this.cat('/prolist')}>项目介绍</button>
+                <button style={{ backgroundImage: `url(${buttonBackground})` }} onClick={() => this.cat('/prolist')}>公告信息</button>
             </div>
         </div>
     }
